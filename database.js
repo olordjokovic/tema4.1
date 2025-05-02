13c56cfb2caad3a35fb9d280b7330fd11060e08e
rpinnie2931 <olorbomba@gmail.com>
2025-05-02 10:25:45 +0200

    Implementado modelo concesionarios y endpoints iniciales

--- database.js
+++ database.js
@@ -1,17 +1,17 @@
 // database.js
 const mongoose = require("mongoose");
 
-const uri = "mongodb+srv://olordjokovic:cerrojo120@cluster0.njnd2dn.mongodb.net/concesionarios?retryWrites=true&w=majority&appName=Cluster0
-"; // Cambia si es necesario
+const uri = "mongodb+srv://olordjokovic:cerrojo120@cluster0.njnd2dn.mongodb.net/concesionarios?retryWrites=true&w=majority&appName=Cluster0"; // Cambia si es necesario
 
 const connectDB = async () => {
   try {
     await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log("🟢 Conectado a MongoDB");
+    console.log("Conectado a MongoDB");
   } catch (err) {
-    console.error("🔴 Error de conexión:", err);
+    console.error("Error de conexión a MongoDB:", err);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
